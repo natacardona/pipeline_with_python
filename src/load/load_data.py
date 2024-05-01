c4a8aced9ebfaadfc00a6331cac4241cdea4eab7
natcardon <natathemilk@gmail.com>
2024-04-30 20:12:17 -0500

    updated hardcoded routes

--- src/load/load_data.py
+++ src/load/load_data.py
@@ -3,26 +3,29 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data_to_db(file_path, engine):
-
+    # Load CSV data into DataFrame
     data = pd.read_csv(file_path)
-
+    # Insert data into the database
     data.to_sql('transactions', con=engine, if_exists='append', index=False)
     print(f"data loaded from {file_path}")
 
 def main():
-
     DATABASE = 'datachallenge'
     USER = 'postgres'
     PASSWORD = 'postgres'
     HOST = 'localhost'
     PORT = '5432'
+    # Create a connection to the database
     engine = create_engine(f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
 
+    # Get the base directory of the project
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+    # Define the directory where CSV files are stored
+    data_directory = os.path.join(base_dir, 'data', 'raw')
 
-    data_directory = '/Users/cardonita/Documents/Development/etl_project/data/raw/dataPruebaDataEngineer'
-
-
-    csv_files = ['2012-1.csv', '2012-2.csv', '2012-3.csv', '2012-4.csv', '2012-5.csv'] 
+    # List of CSV files to process
+    csv_files = ['2012-1.csv', '2012-2.csv', '2012-3.csv', '2012-4.csv', '2012-5.csv']
+    # Process each CSV file
     for file_name in csv_files:
         file_path = os.path.join(data_directory, file_name)
         load_data_to_db(file_path, engine)
